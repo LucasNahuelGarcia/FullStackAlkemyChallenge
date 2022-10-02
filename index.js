9d1c2b34a4f13108e1f8b98fce02c92104b7f93d
LucasNahuelGarcia <nahuellucasgarcia@gmail.com>
2022-10-02 18:42:57 -0300

    added mariadb basic config

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
+const db = require('mariadb');
+const session = require('express-session');
+const mariadb = require('mariadb');
+const MariaDBStore = require('express-session-mariadb-store');
 const app = express();
+
 const port = process.env.PORT || 3000;
+ const pool = mariadb.createPool({ host: process.env.DB_HOST, user: process.env.DB_USER, connectionLimit: 5 });
 
 app.use(morgan('tiny'));
 
-app.get('/', (req, res) => {
-    res.send('Alkemy Challenge');
+app.get('/api/balance', (req, res) => {
+    res.json({
+        balance: 0
+    });
 });
 
 app.listen(port, () => {
