2d8ce1aded9afde55a16bae8ac0eb9b29d5cb040
LucasNahuelGarcia <nahuellucasgarcia@gmail.com>
2022-10-04 18:06:21 -0300

    implemented add transaction operation on the api

--- index.js
+++ index.js
@@ -6,21 +6,21 @@ const session = require('express-session');
 const mariadb = require('mariadb');
 const MariaDBStore = require('express-session-mariadb-store');
 const uuid = require('uuid');
+const path = require('path');
 const app = express();
 
-const options ={
+const options = {
     connectionLimit: 10,
     password: process.env.DB_PASS,
     user: process.env.DB_USER,
-    database: process.env.MYSQL_DB,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    createDatabaseTable: true
+    database: process.env.MYSQL_DB
 }
 
 const port = process.env.PORT || 3000;
 const pool = mariadb.createPool(options);
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'))
 app.use(morgan('tiny'));
 app.use(session({
     store: new MariaDBStore({
@@ -31,9 +31,21 @@ app.use(session({
     secret: uuid.v4(),
 }))
 
-app.get('/api/balance', (req, res) => {
-    res.json({
-        db: process.env.MYSQL_DB,
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/', 'balance/'));
+});
+
+app.post('/api/addTransaction', (req, res) => {
+    console.log(req.body);
+    pool.query("insert into transactions(title, amount, session_id) values(?, ?, ?)", [
+        req.body.title,
+        req.body.amount,
+        req.sessionID
+    ]).then((queryResults) => {
+        res.sendStatus(200);
+    }).catch((err) => {
+        console.log(err);
+        res.sendStatus(500);
     });
 });
 
