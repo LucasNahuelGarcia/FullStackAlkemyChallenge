9fed7b4189c035741a105bfcb9e1de58526496a2
LucasNahuelGarcia <nahuellucasgarcia@gmail.com>
2022-10-05 02:44:41 -0300

    added getTransactions to api

--- index.js
+++ index.js
@@ -31,18 +31,30 @@ app.use(session({
     secret: uuid.v4(),
 }))
 
+app.get('/api/getTransactions', (req, res) => {
+    pool.query("SELECT title,amount FROM transactions WHERE session_id = ?;",
+        [
+            req.sessionID
+        ]).then((queryResults) => {
+            res.json(queryResults);
+        }).catch((err) => {
+            console.log(err);
+            res.sendStatus(500);
+        });
+});
+
 app.post('/api/addTransaction', (req, res) => {
-    console.log(req.body);
-    pool.query("insert into transactions(title, amount, session_id) values(?, ?, ?)", [
-        req.body.title,
-        req.body.amount,
-        req.sessionID
-    ]).then((queryResults) => {
-        res.sendStatus(200);
-    }).catch((err) => {
-        console.log(err);
-        res.sendStatus(500);
-    });
+    pool.query("insert into transactions(title, amount, session_id) values(?, ?, ?)",
+        [
+            req.body.title,
+            req.body.amount,
+            req.sessionID
+        ]).then((queryResults) => {
+            res.sendStatus(200);
+        }).catch((err) => {
+            console.log(err);
+            res.sendStatus(500);
+        });
 });
 
 app.listen(port, () => {
